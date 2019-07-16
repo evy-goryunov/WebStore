6443a0c527ab72423b8870e2eefe4c5162c6703a
evy-goryunov <evy.goryunov@gmail.com>
2019-07-16 20:30:56 +0300

    second

---
+++ WebStore.DomainNew/Entities/Base/Interfaces/IOrderedEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.Domain.Entities.Base.Interfaces
+{
+	public interface IOrderedEntity
+	{
+		int Order { get; set; } // Порядок
+	}
+}
