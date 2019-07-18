285f71bd5bae0a0f5fb3991fc0b28417f4ded8e6
evy_goryunov <evy.goryunov@gmail.com>
2019-07-18 12:16:17 +0300

    four commit

---
+++ WebStore.DAL/WebStoreContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.Entities;
+
+namespace WebStore.DAL
+{
+	class WebStoreContext: DbContext
+	{
+		public WebStoreContext(DbContextOptions options): base(options)
+		{
+
+		}
+		public DbSet<Product> Products { get; set; }
+		public DbSet<Brand> Brands { get; set; }
+		public DbSet<Section> Sections { get; set; }
+
+	}
+}
